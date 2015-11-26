d8377aad11c75de7733e75267e0e3b64c7208dc7
Caleb Dechant <calebdechant@gmail.com>
2015-11-26 13:14:52 -0800

    Edit getWord function, and decrease default memory size
    The getWord was messing up because left shifting bytes greater than 0x7F
    would result in a large negative ints because Java doesn't have unsigned
    bytes.
    
    Default memory size is now 1K.

--- Memory.java
+++ Memory.java
@@ -5,11 +5,18 @@ import java.io.IOException;
 
 public class Memory {
 
-    private byte[] memory;
+    public byte[] memory;
     public int A, X, L, SW, PC;
 
     public Memory() {
-        memory = new byte[32768];
+        // This can be increased if needed. 1K for now because hexdump becomes very large
+        this(1024);
+    }
+    public Memory(int size) {
+        if (size > 32768 || size <= 0) {
+            size = 32768;
+        }
+        memory = new byte[size];
     }
 
     // Get the word in memory at addr; most significant byte is zeroed.
@@ -17,19 +24,25 @@ public class Memory {
         int offset = addr;
         if (indexed)
             offset += X;
-        return (memory[offset] << 16) | (memory[offset + 1] << 8) | memory[offset + 2];
+        int msb = memory[offset];
+        int mid = memory[offset + 1];
+        int lsb = memory[offset + 2];
+        if (msb < 0) msb += 256;
+        if (mid < 0) mid += 256;
+        if (lsb < 0) lsb += 256;
+        return (msb << 16) | mid << 8 | lsb;
     }
 
-    public byte getByte(int addr, boolean indexed) {
+    public int getByte(int addr, boolean indexed) {
         if (indexed)
             return memory[addr + X];
-        return memory[addr];
+        return (int) memory[addr];
     }
 
     // Sets the word in memory at addr to the first 24 bits of val
     public void setWord(int val, int addr, boolean indexed) {
         int offset = addr;
-        if (indexed) 
+        if (indexed)
             offset += X;
         memory[offset]     = (byte) (val >> 16);
         memory[offset + 1] = (byte) (val >> 8);
@@ -39,17 +52,15 @@ public class Memory {
     public void setByte(int val, int addr, boolean indexed) {
         if (indexed)
             memory[addr + X] = (byte) val;
-        else 
+        else
             memory[addr] = (byte) val;
     }
 
     // Save the memory contents to a file so we can see the state of memory
-    public void hexDump(String filename) { 
+    public void hexDump(String filename) {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(new File(filename)))) {
             bw.write("Registers: \n");
-            bw.write(String.format(" A: %06x\t  X: %06x\t  L: %06x\n", A, X, L));
-            bw.write(String.format("SW: %06x\t PC: %06x\n", SW, PC));
-
+            bw.write(String.format(" A: %06x\t  X: %06x\t  L: %06x\t SW: %06x\t PC: %06x\n", A, X, L, SW, PC));
             for (int i = 0; i < memory.length; i++) {
                 if (i % 16 == 0) bw.write(String.format("\n%04x   ", i));
                 bw.write(String.format("%02x", memory[i]));
@@ -57,7 +68,7 @@ public class Memory {
             }
         }
         catch (IOException e) {
-            // do nothing
+            System.err.println("Could not do hexdump: " + e.getMessage());
         }
     }
 }
